2be3f177c8a53706d72a2b9d894c5b7c545e0a00
Gabriel de Oliveira <deoliveira.gab@gmail.com>
2014-08-18 07:50:06 -0300

    Try codeclimate-test-reporter.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
-require "simplecov" unless ENV["COVERAGE"].nil?
+unless ENV["COVERAGE"].nil?
+  require "codeclimate-test-reporter"
+  SimpleCov.start do
+    formatter SimpleCov::Formatter::MultiFormatter[
+      SimpleCov::Formatter::HTMLFormatter,
+      CodeClimate::TestReporter::Formatter
+    ]
+    add_filter "/spec/"
+  end
+end
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "mtrack"
