04ff8e14e962f80242c5e9cdcda9da3769a2106c
Gabriel de Oliveira <deoliveira.gab@gmail.com>
2014-11-30 19:40:01 -0300

    Add debugger support to console.

--- tasks/console.rb
+++ tasks/console.rb
@@ -2,6 +2,18 @@ desc "Open a console with the #{Bundler::GemHelper.gemspec.name} gem loaded"
 task :console do
   require Bundler::GemHelper.gemspec.name
 
+  if RUBY_VERSION >= "2"
+    begin
+      require "byebug"
+    rescue LoadError # rubocop:disable Lint/HandleExceptions
+    end
+  else
+    begin
+      require "debugger"
+    rescue LoadError # rubocop:disable Lint/HandleExceptions
+    end
+  end
+
   if RUBY_VERSION >= "1.9"
     begin
       require "pry"
