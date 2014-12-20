13e7597d55810ddc7dd5bcb05b30c1d7e34de669
Gabriel de Oliveira <deoliveira.gab@gmail.com>
2014-12-20 20:56:39 -0300

    Update coverage rake task.

--- tasks/coverage.rb
+++ tasks/coverage.rb
@@ -1,9 +1,5 @@
 desc "Run tests and generate coverage report"
 task :coverage do
-  if RUBY_VERSION >= "1.9"
-    ENV["COVERAGE"] = "true"
-  else
-    warn "Ruby version 1.9+ is needed in order to generate coverage report."
-  end
+  ENV["COVERAGE"] = "true"
   Rake::Task[:spec].invoke
 end
